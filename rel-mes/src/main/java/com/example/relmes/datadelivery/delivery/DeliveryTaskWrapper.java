36da1818f434ef5af89cd9f96f9cf60bec5a6918
dawidniezgodka <dawid.niezgodka@gmail.com>
2021-06-25 19:43:21 +0200

    simplified version of producer works

--- rel-mes/src/main/java/com/example/relmes/datadelivery/delivery/DeliveryTaskWrapper.java
+++ rel-mes/src/main/java/com/example/relmes/datadelivery/delivery/DeliveryTaskWrapper.java
@@ -2,16 +2,24 @@ package com.example.relmes.datadelivery.delivery;
 
 import com.example.relmes.commons.repo.WeatherRepo;
 import com.example.relmes.datageneration.documents.MongoWeather;
+import com.example.relmes.datageneration.generator.DataGenerator;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.TaskScheduler;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import javax.annotation.PostConstruct;
 import java.util.List;
 import java.util.concurrent.ScheduledFuture;
 
 @Component
 public class DeliveryTaskWrapper {
+
+    private static final Logger LOGGER = LogManager.getLogger(DeliveryTaskWrapper.class);
 
     private static final short POLL_INTERVAL = 10000;
     private static final short MAX_POLL_COUNT = 100;
@@ -31,13 +39,23 @@ public class DeliveryTaskWrapper {
         this.webClient = webClient;
     }
 
+    @PostConstruct
+    public void init() {
+        start();
+    }
+
     class DeliveryTask implements Runnable {
         @Override
         public void run() {
             List<MongoWeather> data = weatherRepo.getLastX(MAX_POLL_COUNT);
-            webClient
+            LOGGER.info("Pulled {} entries", data.size());
+            Integer answer = webClient
                     .post()
-                    .body()
+                    .body(BodyInserters.fromValue("abc"))
+                    .retrieve()
+                    .bodyToMono(Integer.class)
+                    .block(); // should it block?
+            LOGGER.info("Answer from the server: {}", answer);
 
         }
     }
